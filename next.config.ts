a2bde490513eed918f1b9bda20d856228aaa26d6
Martin Mukoya <martinmukoya@gmail.com>
2026-09-05 22:49:52 +0200

    Trace report font into server deployments

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@fontsource/unifrakturcook/files/unifrakturcook-latin-700-normal.woff",
+    ],
+  },
 };
 
 export default nextConfig;
